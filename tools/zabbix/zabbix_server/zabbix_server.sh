66b8456d58ef130e32ce8998781f2f8db33cab5d
lluc <lluc.gilperez@salesianssarria.cat>
2025-02-14 16:03:21 +0100

    test creando el my.conf

--- tools/zabbix/zabbix_server/zabbix_server.sh
+++ tools/zabbix/zabbix_server/zabbix_server.sh
@@ -32,10 +32,43 @@ log_info() {
 }
 
 # Función para crear la base de datos y el usuario de MySQL
+# create_mysql_database_user() {
+#     local db_name=$1
+#     local db_user=$2
+#     local db_password=$3
+
+#     # Ejecutar las operaciones en MySQL y verificar si fallan
+#     if ! mysql --defaults-file=~/.my.cnf <<EOF
+# CREATE DATABASE IF NOT EXISTS $db_name DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+# CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_password';
+# GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';
+# SET GLOBAL log_bin_trust_function_creators = 1;
+# EOF
+#     then
+#         log_error "Error al ejecutar los comandos MySQL para crear la base de datos $db_name y el usuario $db_user."
+#     fi
+# }
+
 create_mysql_database_user() {
     local db_name=$1
     local db_user=$2
     local db_password=$3
+
+    # Verificar si el archivo .my.cnf existe, si no, crearlo
+    if [ ! -f ~/.my.cnf ]; then
+        echo "El archivo ~/.my.cnf no existe, creándolo con los valores predeterminados."
+        
+        # Crear el archivo .my.cnf con configuraciones predeterminadas
+        cat <<EOF > ~/.my.cnf
+[client]
+user = $db_user
+password = $db_password
+host = localhost
+EOF
+
+        # Asegurarse de que el archivo tiene los permisos correctos
+        chmod 600 ~/.my.cnf
+    fi
 
     # Ejecutar las operaciones en MySQL y verificar si fallan
     if ! mysql --defaults-file=~/.my.cnf <<EOF
@@ -48,6 +81,7 @@ EOF
         log_error "Error al ejecutar los comandos MySQL para crear la base de datos $db_name y el usuario $db_user."
     fi
 }
+
 
 # Comprobar si el usuario es root
 if [ "$(id -u)" -ne 0 ]; then
@@ -81,7 +115,7 @@ create_mysql_database_user "$MYSQL_DB" "$MYSQL_USER" "$MYSQL_PASSWORD"
 
 # Importamos el esquema para la base de datos
 log_info "Importando el esquema de la base de datos de Zabbix..."
-if ! zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz | mysql --defaults-file=$MYSQL_CONF_FILE_PATH "$MYSQL_DB"; then
+if ! zcat /usr/share/zabbix/sql-scripts/mysql/server.sql.gz | mysql --defaults-file=~/.my.cnf "$MYSQL_DB"; then
     log_error "Error al importar el esquema para la base de datos."
 fi
 

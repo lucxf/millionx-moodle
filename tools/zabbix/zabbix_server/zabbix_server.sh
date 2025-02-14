df9b0170d14192016512afeaeb9afd15c3d9cf3a
lluc <lluc.gilperez@salesianssarria.cat>
2025-02-14 16:24:27 +0100

    separado en ficheros git push

--- tools/zabbix/zabbix_server/zabbix_server.sh
+++ tools/zabbix/zabbix_server/zabbix_server.sh
@@ -2,6 +2,7 @@
 
 # Importamos funciones de lib.sh
 . ./tools/lib.sh
+. ./tools/zabbix/zabbix_server/BBDD/mysql.sh
 
 # Paquetes a instalar
 packages_to_install=("zabbix-server-mysql" "zabbix-frontend-php" "zabbix-nginx-conf" "zabbix-sql-scripts" "zabbix-agent" "mysql-server")
@@ -10,78 +11,6 @@ packages_to_install=("zabbix-server-mysql" "zabbix-frontend-php" "zabbix-nginx-c
 MYSQL_USER="ubuser_sql"
 MYSQL_PASSWORD="123456aA."
 MYSQL_DB="zabbix_db"
-
-MYSQL_CONF_FILE_PATH="/home/lucxf/millionx-moodle/tools/zabbix/zabbix_server/.my.cnf"
-
-
-# Función para escribir errores en el log y mostrar el mensaje en rojo
-# log_error() {
-#     # Registrar el error en el archivo de log
-#     echo "$(date) - ERROR: $1" | tee -a $LOGFILE
-#     # Mostrar el error en la terminal en rojo
-#     echo -e "\033[31m$(date) - ERROR: $1\033[0m"
-#     # Detener la ejecución del script
-#     exit 1
-# }
-
-# # Función para registrar mensajes informativos
-# log_info() {
-#     # Registrar el mensaje informativo en el archivo de log
-#     echo "$(date) - INFO: $1" | tee -a $LOGFILE
-#     # Mostrar el mensaje en la terminal en azul
-#     echo -e "\033[34m$(date) - INFO: $1\033[0m"
-# }
-
-# Función para crear la base de datos y el usuario de MySQL
-# create_mysql_database_user() {
-#     local db_name=$1
-#     local db_user=$2
-#     local db_password=$3
-
-#     # Ejecutar las operaciones en MySQL y verificar si fallan
-#     if ! mysql --defaults-file=~/.my.cnf <<EOF
-# CREATE DATABASE IF NOT EXISTS $db_name DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-# CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_password';
-# GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';
-# SET GLOBAL log_bin_trust_function_creators = 1;
-# EOF
-#     then
-#         log_error "Error al ejecutar los comandos MySQL para crear la base de datos $db_name y el usuario $db_user."
-#     fi
-# }
-
-create_mysql_database_user() {
-    local db_name=$1
-    local db_user=$2
-    local db_password=$3
-
-    # Verificar si el archivo .my.cnf existe, si no, crearlo
-    if [ ! -f ~/.my.cnf ]; then
-        echo "El archivo ~/.my.cnf no existe, creándolo con los valores predeterminados."
-        
-        # Crear el archivo .my.cnf con configuraciones predeterminadas
-        cat <<EOF > ~/.my.cnf
-[client]
-user = $db_user
-password = $db_password
-host = localhost
-EOF
-
-        # Asegurarse de que el archivo tiene los permisos correctos
-        chmod 600 ~/.my.cnf
-    fi
-
-    # Ejecutar las operaciones en MySQL y verificar si fallan
-    if ! mysql --defaults-file=~/.my.cnf <<EOF
-CREATE DATABASE IF NOT EXISTS $db_name DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS '$db_user'@'localhost' IDENTIFIED BY '$db_password';
-GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';
-SET GLOBAL log_bin_trust_function_creators = 1;
-EOF
-    then
-        log_error "Error al ejecutar los comandos MySQL para crear la base de datos $db_name y el usuario $db_user."
-    fi
-}
 
 
 # Comprobar si el usuario es root
@@ -111,8 +40,12 @@ for package in "${packages_to_install[@]}"; do
     fi
 done
 
-# Crear la base de datos y el usuario en MySQL
-create_mysql_database_user "$MYSQL_DB" "$MYSQL_USER" "$MYSQL_PASSWORD"
+log_info "Configurando MySQL..."
+chmod +x ./tools/zabbix/zabbix_server/BBDD/mysql.sh
+
+if ! sudo ./tools/zabbix/zabbix_server/BBDD/mysql.sh; then
+    log_error "Error al configurar MySQL."
+fi
 
 # Importamos el esquema para la base de datos
 log_info "Importando el esquema de la base de datos de Zabbix..."
